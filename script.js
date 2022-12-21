2277c46b41407cef4da3708a7508db6450ab4f1b
Lyah Bianca Aquino <75360198+biannx@users.noreply.github.com>
2022-12-21 23:45:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ Array.from(buttons).forEach((button) => {
 
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggler-icon");
+
 let isDark = true;
+
 themeToggleBtn.onclick = () => {
   calculator.classList.toggle("dark");
   themeToggleBtn.classList.toggle("active");
